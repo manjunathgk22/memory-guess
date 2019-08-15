130df16d6d4c3050740e90a39330e79689c917f9
Manjnth <manjuanth.kadlimatti@nobroker.in>
2019-08-16 02:00:39 +0530

    modified server fie

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname,'/index.html'));
-});
+app.get(/.*/, (req, res)=>{res.send(__dirname + '/index.html')})
 
 app.listen(9000);
